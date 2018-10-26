8b723f247c6f3a293a30004a82aefdeca7439cda
Jan <Jan@Joans-MacBook-Air.local>
2018-10-26 14:24:33 +0800

    update ps5

--- PS5/code/src/MySpeedDemon.java
+++ PS5/code/src/MySpeedDemon.java
@@ -3,20 +3,20 @@ import java.util.*;
 
 public class MySpeedDemon {
     // Your code
+//    HashMap<Integer, ArrayList<HashMap<Integer, ArrayList<Character>>>> map = new HashMap<>();
+
+    int name = 0;
+
     public static int speedMatch(String dbfilename) {
         // Your code yas
         int result = 0;
-        try{
+        try {
             // open file
             FileReader readFile = new FileReader(dbfilename);
             BufferedReader bufferedFile = new BufferedReader(readFile);
             String lineOne = bufferedFile.readLine();
             // BufferedReader outputs type String -> need to convert to Integer
             int lineCount = Integer.parseInt(lineOne);
-
-            // keep track of number of equivalent
-            int[] wordCount= new int[lineCount];
-            Arrays.fill(wordCount,0);
 
             // read each line
             List<String> input = new ArrayList<String>();
@@ -26,10 +26,33 @@ public class MySpeedDemon {
             }
 
             // create new HashMap
-            HashMap<String, ArrayList<String>> map = new HashMap<>();
+            HashMap<Integer, ArrayList<HashMap<Integer, ArrayList<Character>>>> map = new HashMap<>();
+//            HashMap<String, ArrayList<String>> map = new HashMap<>();
 
-            // for each word, sort the word and use the sorted word as key for map
+            //
             for (String str : input) {
+                HashMap<Integer, ArrayList<Character>> lineMap = new HashMap<>();
+                for (int i = 0; i < str.length(); i++) {
+                    Integer key = (int) str.charAt(i);
+                    if (lineMap.get(key) != null) {
+                        lineMap.get(key).add(str.charAt(i));
+                    } else {
+                        ArrayList<Character> newChar = new ArrayList<>();
+                        newChar.add(str.charAt(i));
+                        lineMap.put(key, newChar);
+                    }
+                }
+
+                // try using arraylist of array list instead of hashmap of array list see faster
+                Integer mKey = lineMap.hashCode();
+                if (map.get(mKey) != null) {
+                    map.get(mKey).add(lineMap);
+                } else {
+                    ArrayList<HashMap<Integer, ArrayList<Character>>> arrMap = new ArrayList<>();
+                    arrMap.add(lineMap);
+                    map.put(mKey, arrMap);
+                }
+            }
 //                char[] wordArray = str.toCharArray();
 //                Arrays.sort(wordArray);
 //                String key = new String(wordArray).toLowerCase();
@@ -42,24 +65,27 @@ public class MySpeedDemon {
 //                    anagramList.add(str);
 //                    map.put(key, anagramList);
 //                }
-                HashMap<String,>
-            }
+//            }
 
-            for (String i : map.keySet()) {
-                List<String> values = map.get(i);
-                if (values.size() >1) {
+            for (Integer i : map.keySet()) {
+                List<HashMap<Integer, ArrayList<Character>>> values = map.get(i);
+                if (values.size() > 1) {
                     int k = values.size();
-                    result += k * (k-1)/2;
+                    result += k * (k - 1) / 2;
                 }
             }
-
-
-
+//            for (String i : map.keySet()) {
+//                List<String> values = map.get(i);
+//                if (values.size() >1) {
+//                    int k = values.size();
+//                    result += k * (k-1)/2;
+//                }
+//            }
 
 
         } catch (IOException e) {
         }
-        return result + '/n';
+        return result;
     }
 
 //    @Override
@@ -71,19 +97,19 @@ public class MySpeedDemon {
 //        if (this == o) {
 //            return true;
 //        }
-//
-//        return true;
 //    }
 
-//    @Override
-//    public int hashCode() {
-//
-//        return result;
-//    }
+    private int a, b;
+    private int[] array;
 
-    // create a HashMap mapping a key string to a linked list of strings
-    // convert word to sorted and lowercased
-    // what is new line character
+    @Override
+    public int hashCode() {
+        return (31 * (31 * Arrays.hashCode(array) + a)) + b;
+
+        // create a HashMap mapping a key string to a linked list of strings
+        // convert word to sorted and lowercased
+        // what is new line character
+    }
 }
 
 
